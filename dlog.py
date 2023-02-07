898ce782f19cb46f649676f74332aaeea11c1890
Alexandre Faria <alexandreffaria@me.com>
2023-02-06 22:47:06 -0300

    Check mouse idle e salva pra arquivo o log

--- dlog.py
+++ dlog.py
@@ -1,32 +1,46 @@
 import time
 import datetime
 import win32gui
+import json
+import win32api
 
 w = win32gui
 janelaInicial = w.GetWindowText(w.GetForegroundWindow())
 startClock = datetime.datetime.now()
 endClock = datetime.datetime.now()
+last_mouse_movement = win32api.GetTickCount()
+
 
 def confereJanelaAtiva():
     return w.GetWindowText(w.GetForegroundWindow())
 
+
+def mouse_idle_time():
+    return (win32api.GetTickCount() - last_mouse_movement) / 1000.0
+
+
 while True:
     janelaAtiva = confereJanelaAtiva()
-    
+
     if janelaInicial != janelaAtiva:
+        endClock = datetime.datetime.now()
+        duracao = (endClock - startClock).total_seconds()
+        evento = {
+            "janela": janelaInicial,
+            "start": str(startClock),
+            "end": str(endClock),
+            "duracao": duracao
+        }
+        if evento["duracao"] >= 10:
+            if mouse_idle_time() < 600:
+                with open('events.json', 'a') as f:
+                    json.dump(evento, f)
+                    f.write('\n')
+            else:
+                print("Evento removido por inatividade")
         startClock = datetime.datetime.now()
         janelaInicial = janelaAtiva
-    
-    else:
-        endClock = datetime.datetime.now()
-
-    duracao = endClock - startClock
-    evento = { 
-        "janela" : janelaAtiva,
-        "duracao" : duracao
-    }
-    
-    print(evento)
+        last_mouse_movement = win32api.GetTickCount()
 
     time.sleep(1)
 
@@ -37,8 +51,6 @@ while True:
 #     print(horaAtual)
 #     print(janelaAtiva)
 #     time.sleep(1)
-
-
 
 
 # current_window = w.GetWindowText(w.GetForegroundWindow())
@@ -60,6 +72,3 @@ while True:
 #     foreground_window_handle = w.GetWindowText(w.GetForegroundWindow())
 #     foreground_window_changed(foreground_window_handle, w.GetWindowText(w.GetForegroundWindow()))
 #     time.sleep(0.5)
-
-
-
