8dee4be7c7b631f0e7301b840c20f8e6e7c8c881
Alexandre Faria <alexandreffaria@me.com>
2023-02-09 07:50:19 -0300

    Removing old code

--- dlog.py
+++ dlog.py
@@ -43,32 +43,3 @@ while True:
         last_mouse_movement = win32api.GetTickCount()
 
     time.sleep(1)
-
-
-# while True:
-#     janelaAtiva = w.GetWindowText(w.GetForegroundWindow())
-#     horaAtual = datetime.datetime.now()
-#     print(horaAtual)
-#     print(janelaAtiva)
-#     time.sleep(1)
-
-
-# current_window = w.GetWindowText(w.GetForegroundWindow())
-# start_time = None
-
-# def foreground_window_changed(prev_foreground_window, new_foreground_window):
-#     global current_window, start_time
-
-#     if prev_foreground_window != new_foreground_window:
-#         if current_window:
-#             print(f"Duration spent on {current_window}: {time.time() - start_time} seconds")
-
-#         current_window = win32gui.GetWindowModuleFileName(new_foreground_window)
-#         start_time = time.time()
-
-# while True:
-#     print(current_window)
-#     print(start_time)
-#     foreground_window_handle = w.GetWindowText(w.GetForegroundWindow())
-#     foreground_window_changed(foreground_window_handle, w.GetWindowText(w.GetForegroundWindow()))
-#     time.sleep(0.5)
